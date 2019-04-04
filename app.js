7f5fdedb7233df594f5cbf51c29c553d9152d281
Jiell Richardson <32615427+jdonric@users.noreply.github.com>
2019-04-03 23:12:57 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+const PORT = 2500;
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.listen(PORT, function (req, res){
+    console.log(`TUNED INTO ${PORT}`);
+});
+
